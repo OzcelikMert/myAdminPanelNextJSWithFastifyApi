d7881c6c69a34c5a52bae56443382d70f558c2a4
Mert ÖZÇELİK <mert.ozcelikk@hotmail.com>
2024-02-19 18:01:19 +0300

    scss

--- src/components/tools/footer.tsx
+++ src/components/tools/footer.tsx
@@ -4,8 +4,8 @@ class ComponentToolFooter extends Component {
   render () {
     return (
       <footer className="footer">
-        <div className="d-sm-flex justify-content-center justify-content-sm-between py-2">
-          <span className="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © {new Date().getFullYear()} <a href="https://www.ozceliksoftware.com/" target="_blank" rel="noopener noreferrer">Ozcelik Software </a></span>
+        <div className="d-sm-flex justify-content-center py-2">
+          <span className="text-center text-sm-left d-block d-sm-inline-block">Copyright © {new Date().getFullYear()} <a href="https://www.ozceliksoftware.com/" target="_blank" rel="noopener noreferrer">Ozcelik Software </a></span>
         </div>
       </footer> 
     );
